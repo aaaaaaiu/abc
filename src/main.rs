aae8e82e937f90c07824b0ab89a51ca0ef4feaed
mochio <mocchioo7@gmail.com>
2021-11-28 22:51:06 +0900

    Add panic

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-use std::time;
-
-struct Timer {
-    prev_time: time::Instant,
-}
-
-impl Timer {
-    fn new() -> Self {
-        Self {
-            prev_time: time::Instant::now(),
-        }
-    }
-    fn elapsed_since_last_call(&mut self) -> time::Duration {
-        let elapsed = self.prev_time.elapsed();
-        println!("{:?}", elapsed);
-        self.prev_time = time::Instant::now();
-        elapsed
-    }
-}
+mod game;
+mod timer;
 
 fn main() {
-    let mut timer = Timer::new();
-    loop {
-        let a = timer.elapsed_since_last_call();
-    }
+    let mut game = game::Game::new();
+    game.run();
 }
